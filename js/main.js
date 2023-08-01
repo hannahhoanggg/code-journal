9418f7366aee2f7f43e61a0f1976744696e88170
Hannah Hoang <hannahhoanggg@gmail.com>
2023-08-01 19:24:42 +0000

    Work in progress still.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const $titleBoxInput = document.querySelector('.title-text');
+const $imageInput = document.querySelector('.image-text');
+const $notesInput = document.querySelector('.notes-text');
+const $image = document.querySelector('img');
+const $form = document.querySelector('.form');
+
+$imageInput.addEventListener('input', updateImage);
+
+function updateImage(event) {
+  const photoURL = $imageInput.value;
+  $image.setAttribute('src', photoURL);
+}
+
+$form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  const formValues = {
+    imageUrl: $imageInput.value,
+    title: $titleBoxInput.value,
+    notes: $notesInput.value,
+    entryId: data.nextEntryId,
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(formValues);
+  $form.reset();
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+}
